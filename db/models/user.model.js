69430806346c7ac33d0535cb155f7db9284e7727
oier <oieruni22@gmail.com>
2024-03-19 17:11:32 +0100

    [NEW] Creating a schema and model for users using sequelize

---
+++ db/models/user.model.js
@@ -0,0 +1,50 @@
+const { Model, DataTypes, Sequelize } = require('sequelize')
+
+const USER_TABLE = 'users';
+
+const UserSchema = {
+  id: {
+    allowNull: false,
+    autoIncrement: true,
+    primaryKey: true,
+    type: DataTypes.INTEGER
+  },
+  email: {
+    allowNull: false,
+    type: DataTypes.STRING,
+    //campo único
+    unique: true,
+  },
+  password: {
+    allowNull: false,
+    type: DataTypes.STRING,
+  },
+  createdAt: {
+    allowNull: false,
+    type: DataTypes.DATE,
+    //Es para especificar el nombre que se va a poner dentro de la tabla, en JS CamelCase y en SQL con _
+    field: 'create_at',
+    //Recoge el valor de la fecha en el momento que se crea este campo
+    defaultValue: Sequelize.NOW,
+  }
+}
+
+class User extends Model {
+  static associate() {
+    //associate
+  }
+  static config(sequelize) {
+    return {
+      sequelize,
+      tableName: USER_TABLE,
+      modelName: 'User',
+      timestamps: false
+    }
+  }
+}
+
+module.exports = {
+  USER_TABLE,
+  UserSchema,
+  User
+}
