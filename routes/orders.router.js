4109bb0ed273e2275431dbb21719abd07583d079
oier <oieruni22@gmail.com>
2024-04-24 11:50:16 +0200

    Add Order model
    Added Order model and related it to customer.

--- routes/orders.router.js
+++ routes/orders.router.js
@@ -2,8 +2,45 @@ const express = require('express');
 
 const router = express.Router();
 
-router.get('/', (req, res) => {
-  res.json([]);
+const validatorHandler = require('./../middlewares/validator.handler');
+const { createOrderSchema, getOrderSchema } = require('../schemas/order.schema');
+const OrderService = require('../services/order.service');
+
+const service = new OrderService();
+
+router.get('/', async (req, res, next) => {
+  try {
+    const response = await service.find();
+    res.json(response);
+  } catch (error) {
+    next(error);
+  }
 });
+
+router.get('/:id',
+  validatorHandler(getOrderSchema, 'params'),
+  async (req, res, next) => {
+    try {
+      const { id } = req.params;
+      const response = await service.findOne(id);
+      res.json(response);
+    } catch (error) {
+      next(error);
+    }
+  }
+);
+
+router.post('/',
+  validatorHandler(createOrderSchema, 'body'),
+  async (req, res, next) => {
+    try {
+      const body = req.body;
+      const response = await service.create(body);
+      res.status(201).json(response);
+    } catch (error) {
+      next(error);
+    }
+  }
+);
 
 module.exports = router;
--- services/order.service.js
+++ services/order.service.js
@@ -1,19 +1,30 @@
-const boom = require('@hapi/boom');
-
+// const boom = require('@hapi/boom');
+const {models} = require('../libs/sequelize')
 class OrderService {
 
   constructor(){
   }
+
   async create(data) {
-    return data;
+    const newOrder = await models.Order.create(data);
+    return newOrder;
   }
 
   async find() {
-    return [];
+    const response = await models.Order.findAll({
+      include: ['customer']
+    })
+    return response;
   }
 
   async findOne(id) {
-    return { id };
+    const response = await models.Order.findByPk(id, {
+      include: [{
+        association: 'customer',
+        include: ['user']
+      }]
+    })
+    return response;
   }
 
   async update(id, changes) {
