684eec6241af0bade7eb4335e700c9d5431e8738
oier <oieruni22@gmail.com>
2024-05-01 17:28:42 +0200

    Add JWT
    Adding JWT to login service.

--- routes/auth.router.js
+++ routes/auth.router.js
@@ -1,5 +1,7 @@
 const express = require('express');
 const passport = require('passport');
+const jwt = require('jsonwebtoken');
+const {config} = require('../config/config')
 
 const router = express.Router();
 
@@ -8,11 +10,20 @@ require('../utils/auth/')
 router.get('/login',
   passport.authenticate('local', { session: false }),
   async (req, res, next) => {
-  try {
-    res.json(req.user)
-  } catch (error) {
-    next(error);
-  }
+    try {
+      const user = req.user
+      const payload = {
+        sub: user.id,
+        role: user.role
+      }
+      const token = jwt.sign(payload, config.jwtSecret)
+      res.json({
+        user,
+        token
+      })
+    } catch (error) {
+      next(error);
+    }
 });
 
 module.exports = router
