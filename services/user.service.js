52a83c1ceadb92f5e3737061728bc22caa1c55c5
oier <oieruni22@gmail.com>
2024-05-01 09:59:39 +0200

    Add new service
    Adding a new service to find users by their emails.

--- services/user.service.js
+++ services/user.service.js
@@ -33,6 +33,13 @@ class UserService {
     return response;
   }
 
+  async findByEmail(email) {
+    const response = await models.User.findOne({
+      where: {email}
+    });
+    return response;
+  }
+
   async findOne(id) {
     const user = await models.User.findByPk(id);
     if (!user) {
